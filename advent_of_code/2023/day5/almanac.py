d7c08426b97e062565ac78809263341f28a4ef29
Alfred Aquino <onequbit@yahoo.com>
2023-12-27 17:35:48 -0600

    need to compute overlap of ranges

--- advent_of_code/2023/day5/almanac.py
+++ advent_of_code/2023/day5/almanac.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
-from tqdm import tqdm
+import json
 import sys
-from os import get_terminal_size
 from os.path import abspath, dirname
 sys.path.append(dirname(abspath("../..")))
 from advent_of_code import load_input_lines
 from advent_of_code import number_str_to_list
 
-H_BAR = "#" * (get_terminal_size().columns - 2)
 
 PHASES = [
             "soil",
@@ -29,48 +27,24 @@ MAP_SECTIONS = [
                 "humidity-to-location"
             ]
 
-class SeedSpan:
-    def __init__(self, start:int, span:int):
-        self.start = start
-        self.end = start + span
-        self.span = span
-        self._set = set(range(start, start+span))
-    
-    def __add__(self, other):
-        if self._set.issuperset(other):
-            return [self]
-        if self._set.issubset(other):
-            return [other]
-        if self._set.isdisjoint(other):
-            return [self, other]
-        return self._set.union(other)
-    
-    def tolist(self):
-        return list(self._set)
-
-def _next(phase:str):
-    if phase == PHASES[-1]:
-        return None
-    next_phase_index = PHASES.index(phase)+1
-    return PHASES[next_phase_index]
-
-def _last(phase:str):
-    if phase == PHASES[0]:
-        return None
-    last_phase_index = PHASES.index(phase)-1
-    return PHASES[last_phase_index]
-
-
 def parse(lines:list):
-    data = {}
+    data = {"seeds":[], "seeds2": [], "seeds_copy":[]}
     line_num = 0
     while line_num < len(lines):
         line = lines[line_num]
         if line.startswith("seeds:"):
             seeds = line.split(":")[1]
-            data["seeds"] = number_str_to_list(seeds)
+            if not data["seeds"]:
+                data["seeds"] += (number_str_to_list(seeds))
+            seeds_copy = list(data["seeds"])
+            data["seeds_copy"] += seeds_copy
+            print(f"{seeds_copy=}")
+            for number in range(0, len(seeds_copy), 2):
+                seed, span = seeds_copy[number], seeds_copy[number+1]
+                data["seeds2"] += [(seed,span)]
             for seed in data["seeds"]:
                 data[str(seed)] = {section:{} for section in MAP_SECTIONS}
+            
         elif line.endswith("map:"):
             map_name = line.split(" ")[0]
             map_line_num = line_num + 1
@@ -78,27 +52,11 @@ def parse(lines:list):
             while map_line_num < len(lines) and (map_line := lines[map_line_num]):
                 line_num = map_line_num
                 map_data.append(number_str_to_list(map_line))
-                
                 map_line_num += 1
             data[map_name] = map_data
         line_num += 1
-    print(f'{data["seeds"]=}')
+    
     return data
-
-def seed_in_span(seed:int, span_start:int, span_end:int):
-    return seed >= span_start and seed <= span_start+span_end
-
-def get_new_seed_list(data:dict):
-    new_list = []
-    seed_number = 0
-    while seed_number + 1 < len(data["seeds"]):
-        seed = data["seeds"][seed_number]
-        span = data["seeds"][seed_number + 1]
-        new_span = SeedSpan(seed, span)
-        print(new_span)
-        new_list += new_span.tolist()
-        seed_number += 2
-    return list(set(new_list))
 
 def parse_mapping(mapping_name:str):
     [source, destination] = mapping_name.split("-to-")
@@ -106,50 +64,58 @@ def parse_mapping(mapping_name:str):
 
 def get_path(seed:int, mapping:str, map_data:dict):
     mappings = map_data[mapping]
-    for [destination, source, span] in mappings:
-        source_range = range(source, source+span)
-        if seed in source_range:
-            offset = seed - source
+    print(f"get_path: {mappings=}")
+    for [destination, seed_mapping, span] in mappings:
+        map_range = range(seed_mapping, seed_mapping+span)
+        print(f"get_path: {seed=}, {span=}, {mapping=}, {map_range=}")
+        if seed in map_range:
+            offset = seed - seed_mapping
             return destination + offset
     return seed
 
-def intersection(lst1, lst2):
-    return list(set(lst1) & set(lst2))
+def get_path2(seed:int, span:int, mapping:str, map_data:dict):
+    source_range = set(range(seed, seed + span))
+    print(f"get_path2: {seed=}, {span=}, {mapping=}, {source_range=}")
+    for source_seed in source_range:
+        destination = get_path(source_seed, mapping, map_data)
+        if destination == seed:
+            continue
+        return destination
+    return seed
 
-
-def part_1(data:dict):
-    print("part_1")
+if __name__ == "__main__":
+    
+    lines = load_input_lines(sys.argv[1])
+    data = parse(lines)
+    print(f"{data=}")
+    print("Part 1: " + "-"*100)
+    print(f"{data['seeds']=}")
+    print(f"size: {len(data['seeds'])}")
+    seed_paths = {}
     for seed in data["seeds"]:
         source = seed
         for section in MAP_SECTIONS:
-            data[str(seed)][section] = get_path(source, section, data)
-            source = data[str(seed)][section]
-    destinations = [data[str(seed)][MAP_SECTIONS[-1]] for seed in data["seeds"]]
+            destination = get_path(source, section, data)
+            data[str(seed)][section] = destination
+            source = destination
+    destinations = [data[str(seed)][MAP_SECTIONS[-1]] for seed in list(data["seeds"])] 
     lowest_location = min(destinations)
     print(f"{destinations=}")
     print(f"{lowest_location=}")
+    # *** puzzle answer: 88151870
     
-    
-def part_2(data:dict):
-    print("part_2")
-    new_seed_list = [data["seeds"][i] for i, _ in enumerate(data["seeds"]) if i % 2 == 0]
-    new_seed_span = [data["seeds"][i] for i, _ in enumerate(data["seeds"]) if i % 2 == 1]
-    seed_ranges = [(seed, span) for seed, span in zip(new_seed_list, new_seed_span)] 
-    all_seeds = {}
-    for seed, span in tqdm(seed_ranges):
-        all_seeds[(seed,span)] = range(seed, seed+span)
-        print(f"({seed=},{span=}): {all_seeds[(seed,span)]=}")
-    largest_range = max(new_seed_span)
-    print(f"{largest_range=}")
-    largest_seed_start = new_seed_list[new_seed_span.index(largest_range)]
-    largest_span = all_seeds[(largest_seed_start,largest_range)]
-    print(f"{largest_span=}")
-
-if __name__ == "__main__":
-    lines = load_input_lines(sys.argv[1])
-    data = parse(lines)
-    part_1(data)
-    print(H_BAR)
-    part_2(data)
-    
-    
+    # print("Part 2: " + "-"*100)
+    # print(f"{data['seeds2']=}")
+    # print(f"size: {len(data['seeds2'])}")
+    # for seed2, span in data["seeds2"]:
+    #     print(f"seeds2 loop: {seed2=}, {span=}...")
+    #     source = seed2
+    #     for section in MAP_SECTIONS:
+    #         destination = get_path2(source, span, section, data)
+    #         data[str(seed2)][section] = destination
+    #         source = destination
+        
+    # destinations2 = [data[str(seed)][MAP_SECTIONS[-1]] for seed,_ in data["seeds2"]]
+    # lowest_location2 = min(destinations2)
+    # print(f"{destinations2=}")
+    # print(f"{lowest_location2=}")
