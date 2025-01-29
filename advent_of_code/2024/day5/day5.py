e4f3d9acba9879249177783172f5261a078be369
Alfred Aquino <alfredaquino@hotmail.com>
2025-01-29 16:28:38 -0500

    day5 part 2 in progress

--- advent_of_code/2024/day5/day5.py
+++ advent_of_code/2024/day5/day5.py
@@ -77,6 +77,22 @@ Of course, you'll need to be careful: the actual list of page ordering rules is 
 
 Determine which updates are already in the correct order. What do you get if you add up the middle page number from those correctly-ordered updates?
 
+Answer: 5713
+
+--- Part Two ---
+
+While the Elves get to work printing the correctly-ordered updates, you have a little time to fix the rest of them.
+
+For each of the incorrectly-ordered updates, use the page ordering rules to put the page numbers in the right order. For the above example, here are the three incorrectly-ordered updates and their correct orderings:
+
+75,97,47,61,53 becomes 97,75,47,61,53.
+61,13,29 becomes 61,29,13.
+97,13,75,29,47 becomes 97,75,47,29,13.
+After taking only the incorrectly-ordered updates and ordering them correctly, their middle page numbers are 47, 29, and 47. Adding these together produces 123.
+
+Find the updates which are not in the correct order. What do you get if you add up the middle page numbers after correctly ordering just those updates?
+
+
 """
 
 from icecream import ic
@@ -94,33 +110,72 @@ def load_data(filename):
 
     return rules, updates
 
-def part_one():
-    rules, updates = load_data("day5_input.txt")
+def get_rule_lookup(rules):
     rule_lookup = {}
     for before, after in rules:
         if before not in rule_lookup:
             rule_lookup[before] = [after]
         else:
             rule_lookup[before].append(after)
+    return rule_lookup
+
+def filter_sequences(rules, updates, rule_lookup):
     valid_updates = []
+    invalid_updates = []
     for sequence in updates:
         is_valid = True
-        for index, number in enumerate(sequence[:-1]):
-            before, after = number, sequence[index+1]
-            if before in rule_lookup[after]:
-                is_valid = False
-                ic("input:", before, after)
-                print(f"{rule_lookup[after]=}")
-                print(sequence)
-                break
+        for index, _ in enumerate(sequence[:-1]):
+            before, after = sequence[index], sequence[index+1]
+            try:
+                if before in rule_lookup[after]:
+                    is_valid = False
+                    # ic("input:", before, after)
+                    # print(f"{rule_lookup[after]=}")
+                    # print(sequence)
+                    break
+            except:
+                pass
         if is_valid:
             valid_updates.append(sequence)
-    print(f"{valid_updates=}")
+        else:
+            invalid_updates.append(sequence)
+    return (valid_updates, invalid_updates)
+
+def part_one(input_file):
+    rules, updates = load_data(input_file)
+    rule_lookup = get_rule_lookup(rules)
+    (valid_updates, invalid_updates) = filter_sequences(rules, updates, rule_lookup)
+    # print(f"{valid_updates=}")
     middles = [sequence[len(sequence)//2] for sequence in valid_updates]
+    # print(f"{middles=}")
+    total = sum(middles)
+    print(f"part one {total=}")
+
+def part_two(input_file):
+    rules, updates = load_data(input_file)
+    rule_lookup = get_rule_lookup(rules)
+    (valid_updates, invalid_updates) = filter_sequences(rules, updates, rule_lookup)
+    
+    for sequence in invalid_updates:
+        print(f"{sequence=}")
+        for index, number in enumerate(sequence[:-1]):
+            before, after = sequence[index], sequence[index+1]
+            try:
+                print(f"{before=}, {after=}, {rule_lookup[after]}")
+                if before in rule_lookup[after]:
+                    sequence[index], sequence[index+1] = sequence[index+1], sequence[index]
+            except:
+                pass
+        print(f"fixed: {sequence}")
+    
+    middles = [sequence[len(sequence)//2] for sequence in invalid_updates]
     print(f"{middles=}")
     total = sum(middles)
-    print(f"{total=}")
-    
+    print(f"part two {total=}")
+    # 5713 <-- too high
+
 
 if __name__ == "__main__":
-    part_one() # 5713
+    input_file = "day5_sample.txt"
+    part_one(input_file) # 5713
+    part_two(input_file)
